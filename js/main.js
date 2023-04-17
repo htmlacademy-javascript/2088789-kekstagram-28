e408428ecf363ae501356a005660a0059d902712
Rodion <103366016+ZhRodion@users.noreply.github.com>
2023-04-17 22:30:35 +0400

    Пуш законченного ДЗ

--- js/main.js
+++ js/main.js
@@ -1,11 +1,10 @@
-import { createPhotoInform } from './data.js';
 import { modifiedPhotoList } from './photo-render.js';
 import { modifiedCommentList } from './big-picture.js';
-import { createComments } from './data.js';
+
 import './pop-up.js';
 import './user-form.js';
 
 
-modifiedPhotoList(createPhotoInform());
-modifiedCommentList(createComments());
+modifiedPhotoList();
+modifiedCommentList();
 
--- js/util.js
+++ js/util.js
@@ -1,3 +1,5 @@
+const ALERT_SHOW_TIME = 5000;
+
 // Функция рандомизации в массиве
 function getRandomNumber(a, b) {
   const randomDigit = a + Math.random() * (b + 1 - a);
@@ -10,5 +12,29 @@ function getRandomArrayElement(arr) {
   const randomValue = arr[randomIndex];
   return randomValue;
 }
+
+// Аллерт
+const showAlert = (message) => {
+  const alertContainer = document.createElement('div');
+  alertContainer.style.zIndex = 100;
+  alertContainer.style.position = 'absolute';
+  alertContainer.style.top = 0;
+  alertContainer.style.left = 0;
+  alertContainer.style.right = 0;
+  alertContainer.style.padding = '10px 3px';
+  alertContainer.style.fontSize = '30px';
+  alertContainer.style.textAlign = 'center';
+  alertContainer.style.backgroundColor = 'pink';
+
+  alertContainer.textContent = message;
+
+  document.body.append(alertContainer);
+
+  setTimeout(() => {
+    alertContainer.remove();
+  }, ALERT_SHOW_TIME);
+};
+
 export { getRandomArrayElement };
 export { getRandomNumber };
+export { showAlert };
